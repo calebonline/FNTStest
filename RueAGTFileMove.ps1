cc4cb95c02bd818a1c910202b43725a773e65b4d
calebonline <caleb.fili@gmail.com>
2021-10-24 03:34:26 -0500

    Add Rue arguments

--- RueAGTFileMove.ps1
+++ RueAGTFileMove.ps1
@@ -1,3 +1,5 @@
-## This is working in the dev environment and needs reworked for the Rue server
-Move-Item -Path '\\OMAFS01v\Z$\Data\Testloc2\test1.txt' -Destination '\\OMAFS01v\Z$\Data\Testloc1'
-Move-Item -Path '\\OMAFS01v\Z$\Data\Testloc2\test2.txt' -Destination '\\OMAFS01v\Z$\Data\Testloc1'
+## The commented out portion is working in the dev environment and needs reworked for the Rue server
+Move-Item -Path '\\OMAFS01v\Z$\Data\Test loc2\test1.txt' -Destination '\\OMAFS01v\Z$\Data\Test loc1'
+Move-Item -Path '\\OMAFS01v\Z$\Data\Test loc2\test2.txt' -Destination '\\OMAFS01v\Z$\Data\Test loc1'
+##Move-Item -Path '\\ruemulepa1\MulePA1_archives\ATGextract\sales_line_header_*' -Destination 'H:\MPA Admin\KIBO Archive'
+##Move-Item -Path '\\ruemulepa1\MulePA1_archives\ATGextract\shipment_logs_bopis_*' -Destination 'H:\MPA Admin\KIBO Archive'
