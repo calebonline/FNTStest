35757ca2ca94698ea38f4d2cf7f31be2a132869e
calebonline <caleb.fili@gmail.com>
2021-10-24 03:24:59 -0500

    Proof of Concept!

--- RueAGTFileMove.ps1
+++ RueAGTFileMove.ps1
@@ -1,4 +1,3 @@
-Move-Item -Path \\ruemulepa1\MulePA1_archives\ATGExtract\sales_lines_header_*.csv -Destination H:\MPA Admin\KIBO Archive
-
-test from surface
-
+## This is working in the dev environment and needs reworked for the Rue server
+Move-Item -Path '\\OMAFS01v\Z$\Data\Testloc2\test1.txt' -Destination '\\OMAFS01v\Z$\Data\Testloc1'
+Move-Item -Path '\\OMAFS01v\Z$\Data\Testloc2\test2.txt' -Destination '\\OMAFS01v\Z$\Data\Testloc1'
